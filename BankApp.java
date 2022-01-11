d3a4270957c97b10d817e32c83229158d782d805
pavanpaps <pavanishot200@gmail.com>
2022-01-11 13:20:57 +0530

    add whatever i have done

---
+++ BankApp.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+class BankApp {
+    String name;
+    int addr;
+    int accno;
+    String type;
+    int bal;
+    static Scanner sc;
+
+    public BankApp(Scanner scanner) {
+        System.out.print("Enter your name");
+        name = sc.nextLine();
+        System.out.print("Enter your address");
+        addr = sc.nextInt();
+        System.out.print("Enter your acc no");
+        accno = sc.nextInt();
+        System.out.print("Enter your account type");
+        type = sc.nextLine();
+        System.out.print("Enter your balance");
+        bal = sc.nextInt();
+        sc = scanner;
+    }
+
+    public void deposit() {
+        System.out.println("Enter the amount to be deposited");
+        int amt = sc.nextInt();
+        bal = bal + amt;
+        System.out.println("Thank you for depositing");
+        System.out.println("Your current balance is = " + bal);
+    }
+
+    public void withdraw() {
+        System.out.println("Enter the amount to be Withdrawn");
+        int amt = sc.nextInt();
+        bal = bal - amt;
+        System.out.println("Please collect your cash");
+        System.out.println("Your current balance is = " + bal);
+    }
+
+    public void checkbal() {
+        System.out.println("Account number= " + accno + "\nBalance= " + bal);
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Address: " + addr);
+        System.out.println("Account type: " + type);
+        System.out.println("Account Number: " + accno);
+        System.out.println("Balance: " + bal);
+    }
+
+    public static void main(String args[]) {
+        Scanner inp = new Scanner(System.in);
+        BankApp cu = new BankApp(inp);
+        System.out.println("Welcome to the banking application! Your details are given below");
+        cu.display();
+        int opt;
+        char ch;
+        boolean keepOnGoing;
+        do {
+            System.out.println("Enter an option");
+            opt = inp.nextInt();
+            switch (opt) {
+                case 1:
+                    cu.deposit();
+                    break;
+                case 2:
+                    cu.withdraw();
+                    break;
+                case 3:
+                    cu.checkbal();
+                    break;
+                case 4:
+                    cu.display();
+                    break;
+                default:
+                    System.out.println("Invalid Option");
+            }
+            System.out.println("Would you like to continue?");
+            ch = Character.toLowerCase(inp.next().charAt(0));
+            if (ch == 'y') {
+                keepOnGoing = true;
+            } else if (ch == 'n') {
+                keepOnGoing = false;
+            } else {
+                System.out.println("Invalid Response exiting !!!");
+                keepOnGoing = false;
+            }
+        } while (keepOnGoing);
+        inp.close();
+    }
+}
---
+++ Pixel.java
@@ -0,0 +1,65 @@
+// Java program change RGB Color
+// Model to HSV Color Model
+import java.util.Scanner;
+
+class Pixel {
+
+    public void HSVcalculator(Scanner sc) {
+
+        // R, G, B values are divided by 255
+        // to change the range from 0 to 255 to 0 to 1
+
+        System.out.print("Enter Red Colour: ");
+        double r = sc.nextDouble();
+        r = r / 255.0;
+
+        System.out.print("Enter Green Colour: ");
+        double g = sc.nextDouble();
+        g = g / 255.0;
+
+        System.out.print("Enter Blue Colour: ");
+        double b = sc.nextDouble();
+        b = b / 255.0;
+
+        // h, s, v = hue, saturation, value
+        double max = Math.max(r, Math.max(g, b)); // maximum of r, g, b
+        double min = Math.min(r, Math.min(g, b)); // minimum of r, g, b
+        double diff = max - min; // diff of max and min.
+        double h = 0, s = 0;
+
+        if (max == min)
+            h = 0;
+
+        // if max equal r then compute h
+        else if (max == r)
+            h = (60 * ((g - b) / diff) + 360) % 360;
+
+        // if max equal g then compute h
+        else if (max == g)
+            h = (60 * ((b - r) / diff) + 120) % 360;
+
+        // if max equal b then compute h
+        else if (max == b)
+            h = (60 * ((r - g) / diff) + 240) % 360;
+
+        // if max equal zero
+        if (max == 0)
+            s = 0;
+        else
+            s = (diff / max) * 100;
+
+        // compute v
+        double v = max * 100;
+
+        // Print all the stuff
+        System.out.println("Hue (H): " + h + " degrees");
+        System.out.println("Saturation (S): " + s + "%");
+        System.out.println("Value (V): " + v + "%");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pixel inp = new Pixel();
+        inp.HSVcalculator(sc);
+    }
+}
